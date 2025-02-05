0874ff048cb09b7a28a47059f7d4448f11c0f736
xio88oix <96489840+xio88oix@users.noreply.github.com>
2025-02-05 07:08:40 -0500

    Update HomeController.java

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -1,12 +1,20 @@
 package com.example;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+    @GetMapping("/api/")
+    public String home(Model model) {
+        model.addAttribute("message", "Spring MVC Test");
+        return "index";
+    }
+    
     @RequestMapping("/")
     @ResponseBody
     public String home() {
